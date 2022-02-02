9e0e4007db82010124b3d8a6ab7ce090b42abd5a
Teethew <thiagjack@gmail.com>
2022-02-02 00:45:24 -0300

    writing and reading from file

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/teethew/protobuf
+
+go 1.17
+
+replace github.com/teethew/protobuf => ./
+
+require google.golang.org/protobuf v1.27.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/teethew/protobuf/src/simple"
+	"google.golang.org/protobuf/proto"
+)
 
 func main() {
-	fmt.Println("hello world")
+	
+	sm := doSimple()
+	writeToFile("simple.bin", sm)
+
+	sm2 := &simplepb.SimpleMessage{}
+	readFromFile("simple.bin", sm2)
+
+	fmt.Println(sm2)
+}
+
+func writeToFile(fname string, pb proto.Message) error {
+	out, err := proto.Marshal(pb)
+	if err != nil {
+		log.Fatalln("Can't serialize to bytes", err)
+		return err
+	}
+
+	if err := ioutil.WriteFile(fname, out, 0644); err != nil {
+		log.Fatalln("Can't write to file", err)
+		return err
+	}
+
+	fmt.Printf("Data has been written to the file %s!\n", fname)
+	return nil
+}
+
+func readFromFile(fname string, pb proto.Message) error {
+	in, err := ioutil.ReadFile(fname)
+
+	if err != nil {
+		log.Fatalln("Can't read from file", err)
+		return err
+	}
+
+	
+	if err := proto.Unmarshal(in, pb); err != nil {
+		log.Fatalln("Can't desserialize to string", err)
+		return err
+	}
+
+
+	fmt.Printf("Data has been read from the file %s!\n", fname)
+	return nil
+}
+
+func doSimple() *simplepb.SimpleMessage {
+	sm := simplepb.SimpleMessage{
+		Id: 1,
+		IsSimple: true,
+		Name: "Message created successfully",
+		SampleList: []int32 {7, 0, 0, 4},
+	}
+
+	fmt.Println(sm.String())
+
+	sm.Name = "The protocol buffers methods are working fine"
+
+	return &sm
 }
